a870bc450d92b5192414a7adfe4071d37bfdfd8a
Jacky Tea <teajacks@outlook.com>
2020-06-09 17:13:27 -0400

    switch project card order

--- src/components/ProjectsPage/ProjectsPage.js
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -35,30 +35,6 @@ class ProjectsPage extends Component {
         <div className="ProjectContainer">
           <div className="ProjectCard">
             <div className="ProjectLogo">
-              <img src={RealTimePolling} alt="Real Time Polling Demo"></img>
-            </div>
-            <div className="ProjectDescription">
-              <h1>Real Time Polling Application</h1>
-              <ul>
-                <li><NodeJSIcon></NodeJSIcon> &nbsp;Node.JS</li>
-                <li><MongoIcon></MongoIcon> &nbsp;MongoDB</li>
-                <li><PusherIcon></PusherIcon> &nbsp;Pusher API</li>
-              </ul>
-              <p>An asynchronous web application that updates changes to polling results without having to refresh the page.</p>
-            </div>
-            <div className="LinkCard">
-              <a href="https://sheltered-inlet-92987.herokuapp.com/" target="_blank"
-                rel="noopener noreferrer">
-                <button aria-label="live demo">Live Demo &nbsp;<VisitIcon></VisitIcon></button></a>
-              <a href="https://github.com/JackyTea/Real-Time-Polling-Application" target="_blank"
-                rel="noopener noreferrer" className="gitLink">
-                <button aria-label="view source">Source Code &nbsp;<GithubIcon></GithubIcon></button></a>
-              <p className="warning"><b>Notice: </b>Heroku site might take time to connect. Find out why <a href="https://blog.heroku.com/app_sleeping_on_heroku" target="_blank"
-                rel="noopener noreferrer">here.</a></p>
-            </div>
-          </div>
-          <div className="ProjectCard">
-            <div className="ProjectLogo">
               <img src={GridLock} alt="GridLock Pathfinder Demo"></img>
             </div>
             <div className="ProjectDescription">
@@ -75,6 +51,28 @@ class ProjectsPage extends Component {
                 rel="noopener noreferrer">
                 <button aria-label="live demo">Live Demo &nbsp;<VisitIcon></VisitIcon></button></a>
               <a href="https://github.com/JackyTea/Gridlock-Pathfinder" target="_blank"
+                rel="noopener noreferrer" className="gitLink">
+                <button aria-label="view source">Source Code &nbsp;<GithubIcon></GithubIcon></button></a>
+            </div>
+          </div>
+          <div className="ProjectCard">
+            <div className="ProjectLogo">
+              <img src={MakeItRain} alt="Make It Rain Demo"></img>
+            </div>
+            <div className="ProjectDescription">
+              <h1>DOM Manipulation Browser Game</h1>
+              <ul>
+                <li><NodeJSIcon></NodeJSIcon> &nbsp;Node.JS</li>
+                <li><HTMLIcon></HTMLIcon> &nbsp;HTML5</li>
+                <li><CSSIcon></CSSIcon> &nbsp;CSS3</li>
+              </ul>
+              <p>A JavaScript browser game that takes advantage of mouse events to drop falling objects from where the user clicks.</p>
+            </div>
+            <div className="LinkCard">
+              <a href="https://jackytea.github.io/GH_Make_It_Rain/" target="_blank"
+                rel="noopener noreferrer">
+                <button aria-label="live demo">Live Demo &nbsp;<VisitIcon></VisitIcon></button></a>
+              <a href="https://github.com/JackyTea/Make-It-Rain" target="_blank"
                 rel="noopener noreferrer" className="gitLink">
                 <button aria-label="view source">Source Code &nbsp;<GithubIcon></GithubIcon></button></a>
             </div>
@@ -105,24 +103,26 @@ class ProjectsPage extends Component {
           </div>
           <div className="ProjectCard">
             <div className="ProjectLogo">
-              <img src={MakeItRain} alt="Make It Rain Demo"></img>
+              <img src={RealTimePolling} alt="Real Time Polling Demo"></img>
             </div>
             <div className="ProjectDescription">
-              <h1>DOM Manipulation Browser Game</h1>
+              <h1>Real Time Polling Application</h1>
               <ul>
                 <li><NodeJSIcon></NodeJSIcon> &nbsp;Node.JS</li>
-                <li><HTMLIcon></HTMLIcon> &nbsp;HTML5</li>
-                <li><CSSIcon></CSSIcon> &nbsp;CSS3</li>
+                <li><MongoIcon></MongoIcon> &nbsp;MongoDB</li>
+                <li><PusherIcon></PusherIcon> &nbsp;Pusher API</li>
               </ul>
-              <p>A JavaScript browser game that takes advantage of mouse events to drop falling objects from where the user clicks.</p>
+              <p>An asynchronous web application that updates changes to polling results without having to refresh the page.</p>
             </div>
             <div className="LinkCard">
-              <a href="https://jackytea.github.io/GH_Make_It_Rain/" target="_blank"
+              <a href="https://sheltered-inlet-92987.herokuapp.com/" target="_blank"
                 rel="noopener noreferrer">
                 <button aria-label="live demo">Live Demo &nbsp;<VisitIcon></VisitIcon></button></a>
-              <a href="https://github.com/JackyTea/Make-It-Rain" target="_blank"
+              <a href="https://github.com/JackyTea/Real-Time-Polling-Application" target="_blank"
                 rel="noopener noreferrer" className="gitLink">
                 <button aria-label="view source">Source Code &nbsp;<GithubIcon></GithubIcon></button></a>
+              <p className="warning"><b>Notice: </b>Heroku site might take time to connect. Find out why <a href="https://blog.heroku.com/app_sleeping_on_heroku" target="_blank"
+                rel="noopener noreferrer">here.</a></p>
             </div>
           </div>
           <div className="ProjectCard">

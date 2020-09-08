7e2fdf96e55ebb218137861d0791b5a121ac93ef
Jacky Tea <teajacks@outlook.com>
2020-09-08 14:13:26 -0400

    add new experience

--- src/components/ExperiencePage/ExperiencePage.js
+++ src/components/ExperiencePage/ExperiencePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { ReactComponent as SnakebyteIcon } from "../../images/icons/snakebyteIcon.svg";
 import { ReactComponent as FreeGeekIcon } from "../../images/icons/freegeekIcon.svg";
+import { ReactComponent as IBMIcon } from "../../images/icons/ibmIcon.svg";
 import "./ExperiencePage.css";
 
 
@@ -18,6 +19,19 @@ class ExperiencePage extends Component {
                     <h1>Experience</h1>
                 </div>
                 <div className="ExperienceContainer">
+                <div className="ExperienceCard">
+                        <div className="CompanyLogo" id="snakebyte-studios">
+                            <IBMIcon></IBMIcon>
+                        </div>
+                        <div className="JobDescription">
+                            <h1>Software Developer</h1>
+                            <h2>IBM - Markham, Ontario</h2>
+                        </div>
+                        <div className="DateCard">September 2020 - Present<br />
+                            <a href="https://www.ibm.com/ca-en" target="_blank"
+                                rel="noopener noreferrer">
+                                <button>Company Site</button></a></div>
+                    </div>
                     <div className="ExperienceCard">
                         <div className="CompanyLogo" id="snakebyte-studios">
                             <SnakebyteIcon></SnakebyteIcon>
@@ -26,7 +40,7 @@ class ExperiencePage extends Component {
                             <h1>Software Developer</h1>
                             <h2>Snakebyte Studios Inc. - Toronto, Ontario</h2>
                         </div>
-                        <div className="DateCard">May 2020 - Present<br />
+                        <div className="DateCard">May 2020 - September 2020<br />
                             <a href="https://www.snakebyte.ca/" target="_blank"
                                 rel="noopener noreferrer">
                                 <button>Company Site</button></a></div>

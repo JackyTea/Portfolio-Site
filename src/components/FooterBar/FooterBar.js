7d42bb8aa309e7c12efdb7f0bedcfba31f5a0cbb
Jacky Tea <teajacks@outlook.com>
2021-03-13 17:03:52 -0500

    remove email icon

--- src/components/FooterBar/FooterBar.js
+++ src/components/FooterBar/FooterBar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { ReactComponent as EmailIcon } from "../../images/icons/emailIcon.svg";
 import { ReactComponent as GithubIcon } from "../../images/icons/githubIcon.svg";
 import { ReactComponent as LinkedinIcon } from "../../images/icons/linkedinIcon.svg";
 import "./FooterBar.css";

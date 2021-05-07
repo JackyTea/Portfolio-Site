606ecf40baec904c4497261a8960d8dc89bca250
Jacky Tea <teajacks@outlook.com>
2021-05-07 09:57:11 -0400

    Updates

--- precache-manifest.317cbb995befce806d8a5d186a4eae22.js
+++ precache-manifest.317cbb995befce806d8a5d186a4eae22.js
@@ -1,31 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcd89f8a7df64f8ae4a465225484d722",
+    "revision": "ffaecb3f142120b25167f3a88564c8f6",
     "url": "/index.html"
   },
   {
-    "revision": "652927c40baac96abd13",
-    "url": "/static/css/main.c832ea91.chunk.css"
+    "revision": "33a4972b00e4edc7c666",
+    "url": "/static/css/main.dc47d55c.chunk.css"
   },
   {
-    "revision": "a4ab4b7c38b2c8559640",
-    "url": "/static/js/2.2527b1c1.chunk.js"
+    "revision": "46236672ddc662d53c18",
+    "url": "/static/js/2.27db6b02.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/static/js/2.2527b1c1.chunk.js.LICENSE"
+    "url": "/static/js/2.27db6b02.chunk.js.LICENSE"
   },
   {
-    "revision": "652927c40baac96abd13",
-    "url": "/static/js/main.453969c6.chunk.js"
+    "revision": "33a4972b00e4edc7c666",
+    "url": "/static/js/main.2983c7b9.chunk.js"
   },
   {
     "revision": "c9913191e565de632244",
     "url": "/static/js/runtime-main.170f8412.js"
-  },
-  {
-    "revision": "8b32dfbad51e83d4201a8ee5c440409d",
-    "url": "/static/media/Arduino_Square.8b32dfba.svg"
   },
   {
     "revision": "0d0608914cbedb7478a43aa5957cd306",
@@ -40,10 +36,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/GridLock_Square.67d83037.svg"
   },
   {
-    "revision": "85faf67179fdabdcdc16f01c498b14a7",
-    "url": "/static/media/MakeItRain_Square.85faf671.svg"
-  },
-  {
     "revision": "b6b70fb23e57386bb1c87bed2b15c1f0",
     "url": "/static/media/RealTimePolling_Square.b6b70fb2.svg"
   },
@@ -56,16 +48,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/WebGL_Square.a2463783.svg"
   },
   {
-    "revision": "e2f582dd92f79285f5058c4a784447f3",
-    "url": "/static/media/androidIcon.e2f582dd.svg"
-  },
-  {
     "revision": "2f48820f6c967d09fd5e87d68e1dbf7c",
     "url": "/static/media/angularIcon.2f48820f.svg"
-  },
-  {
-    "revision": "863593ddad7fd4e3447884c92d4832c0",
-    "url": "/static/media/arduinoIcon.863593dd.svg"
   },
   {
     "revision": "858862dc232ec8b5f038b3c082f57d91",
@@ -76,32 +60,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/cssIcon.6971ddc7.svg"
   },
   {
-    "revision": "f02160604d42f622610f1424a838c40f",
-    "url": "/static/media/dartIcon.f0216060.svg"
-  },
-  {
-    "revision": "dc13e32a270c8e0c85d126e8e9469643",
-    "url": "/static/media/djangoIcon.dc13e32a.svg"
-  },
-  {
-    "revision": "b04f07292686e29178f004e295b5d4b2",
-    "url": "/static/media/firebaseIcon.b04f0729.svg"
-  },
-  {
-    "revision": "faf8f499559f6b2f815f454a5794eae3",
-    "url": "/static/media/flutterIcon.faf8f499.svg"
-  },
-  {
     "revision": "f17d68774cb90fda1b57188533c609de",
     "url": "/static/media/footerBackground.f17d6877.svg"
   },
   {
     "revision": "4e1b1bdc8a7c6111df87685eb36c11eb",
     "url": "/static/media/footerBackgroundPhone.4e1b1bdc.svg"
-  },
-  {
-    "revision": "098aca5d56724a92bc0073cf701626a6",
-    "url": "/static/media/freegeekIcon.098aca5d.svg"
   },
   {
     "revision": "a36a1b31b1c6dad9949ff11c9a08a8bb",
@@ -116,20 +80,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/ibmIcon.321b4d76.svg"
   },
   {
-    "revision": "ea37f9fe9990771d80b34197c9342fb1",
-    "url": "/static/media/javaIcon.ea37f9fe.svg"
-  },
-  {
-    "revision": "3fd98e7e7b118b7770ecba29c226da0f",
-    "url": "/static/media/jsIcon.3fd98e7e.svg"
-  },
-  {
     "revision": "591e237ac83b89a6d693c36fc625f80a",
     "url": "/static/media/linkedinIcon.591e237a.svg"
-  },
-  {
-    "revision": "590db13f697bb025026642f559b8415b",
-    "url": "/static/media/menuIcon.590db13f.svg"
   },
   {
     "revision": "8e08e1ba858716f1e67b46e4549a8eea",
@@ -144,36 +96,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/opencvIcon.9cdc9c2a.svg"
   },
   {
-    "revision": "eac4eb83f1459e6b9e1c72596b08e0da",
-    "url": "/static/media/postgresqlIcon.eac4eb83.svg"
-  },
-  {
     "revision": "8870783734ab4398805b8f5b689cebfe",
     "url": "/static/media/projSkills.88707837.svg"
   },
   {
     "revision": "fe09938f0d9bf87ca24d04767f0bf9e0",
     "url": "/static/media/pusherIcon.fe09938f.svg"
-  },
-  {
-    "revision": "01766e9cdd6e12e020a2c9ca505fd961",
-    "url": "/static/media/pythonIcon.01766e9c.svg"
-  },
-  {
-    "revision": "fcd0fc5dc4281120b8201a681020af00",
-    "url": "/static/media/railsIcon.fcd0fc5d.svg"
-  },
-  {
-    "revision": "5178d9baf2bdb307096c1f906e2856cd",
-    "url": "/static/media/reactIcon.5178d9ba.svg"
-  },
-  {
-    "revision": "c99d042b5423799cb87316b10c8e5667",
-    "url": "/static/media/rubyIcon.c99d042b.svg"
-  },
-  {
-    "revision": "9e7b015941562c083f33f30b160dd61b",
-    "url": "/static/media/sassIcon.9e7b0159.svg"
   },
   {
     "revision": "42ef0404d13da4c2c716346d4f40e733",
@@ -194,13 +122,5 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ff048ca78b9f0632e60497e33e29a806",
     "url": "/static/media/visitIcon.ff048ca7.svg"
-  },
-  {
-    "revision": "af39691cf9c2a3751693fd0576678de4",
-    "url": "/static/media/vueIcon.af39691c.svg"
-  },
-  {
-    "revision": "0b0e2afb14f1d664baf95bbc441556fd",
-    "url": "/static/media/winsockIcon.0b0e2afb.svg"
   }
 ]);

1db694fe04d29d9f76f200844ed8a6d90adec9fc
Jacky Tea <teajacks@outlook.com>
2022-03-22 20:22:05 -0400

    Updates

---
+++ precache-manifest.a3d873a72e5afc0765742df723ccc82c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db6746ddf5ae78e7a7d5139309bbf27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fefa63d9ba814cdd3fbd",
+    "url": "/static/css/main.12c2c147.chunk.css"
+  },
+  {
+    "revision": "77fe9e9d31d401c54990",
+    "url": "/static/js/2.b21d24db.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/static/js/2.b21d24db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fefa63d9ba814cdd3fbd",
+    "url": "/static/js/main.eb62c40a.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "0d0608914cbedb7478a43aa5957cd306",
+    "url": "/static/media/ComputerVision_Square.0d060891.svg"
+  },
+  {
+    "revision": "67d83037b426c7d2d480d95efba6d31a",
+    "url": "/static/media/GridLock_Square.67d83037.svg"
+  },
+  {
+    "revision": "913a6625634b2fec56f45d558b4f7ef7",
+    "url": "/static/media/Mock_Stocks.913a6625.svg"
+  },
+  {
+    "revision": "a2463783090a8472dfac94989c1b8014",
+    "url": "/static/media/WebGL_Square.a2463783.svg"
+  },
+  {
+    "revision": "f17d68774cb90fda1b57188533c609de",
+    "url": "/static/media/footerBackground.f17d6877.svg"
+  },
+  {
+    "revision": "4e1b1bdc8a7c6111df87685eb36c11eb",
+    "url": "/static/media/footerBackgroundPhone.4e1b1bdc.svg"
+  },
+  {
+    "revision": "a36a1b31b1c6dad9949ff11c9a08a8bb",
+    "url": "/static/media/githubIcon.a36a1b31.svg"
+  },
+  {
+    "revision": "321b4d76c885ab24ffd6c4eb5478970f",
+    "url": "/static/media/ibmIcon.321b4d76.svg"
+  },
+  {
+    "revision": "591e237ac83b89a6d693c36fc625f80a",
+    "url": "/static/media/linkedinIcon.591e237a.svg"
+  },
+  {
+    "revision": "8870783734ab4398805b8f5b689cebfe",
+    "url": "/static/media/projSkills.88707837.svg"
+  },
+  {
+    "revision": "8e232e0a3a876688b9297193a8059bf5",
+    "url": "/static/media/shopifyIcon.8e232e0a.svg"
+  },
+  {
+    "revision": "4125c088777a9a5988f76b9a7b04c54b",
+    "url": "/static/media/topBackground.4125c088.svg"
+  },
+  {
+    "revision": "5d36956a3766912acb2a7e4c43d3f8c2",
+    "url": "/static/media/topBackgroundPhone.5d36956a.svg"
+  },
+  {
+    "revision": "afc6f329d3020cba83ff8bd3570a7ecd",
+    "url": "/static/media/topBackgroundTablet.afc6f329.svg"
+  },
+  {
+    "revision": "ff048ca78b9f0632e60497e33e29a806",
+    "url": "/static/media/visitIcon.ff048ca7.svg"
+  }
+]);
